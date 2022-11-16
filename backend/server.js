2efac6b090eed1b96980a366d8f018c31d4fc2d6
marcelsadge <zjunhao@seas.upenn.edu>
2022-11-16 12:30:56 -0500

    Add MongoDB server.js

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+
+mongoose.connect(uri, {});
+
+app.listen(process.env.PORT, () =>
+  console.log(`MongoDB listening at port: ${port}`)
+);
+
+server.listen(3000, () => {
+  console.log('Socket listening at port: 3000');
+});
+
+module.exports = app;
